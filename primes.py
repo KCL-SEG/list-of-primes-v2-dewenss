5f484db56913a215dc534ebba6603d87175dd78b
Dylan <k22026193@kcl.ac.uk>
2023-10-03 23:35:34 +0100

    Add error message for if the user enters a number below zero.

--- primes.py
+++ primes.py
@@ -3,4 +3,21 @@
 
 def primes(number_of_primes):
     list = []
-    return list
+    count = 2
+    if number_of_primes > 0:
+        while len(list) < number_of_primes:
+            is_prime = True
+            for j in range(2,count):
+                if count%j == 0:
+                    is_prime = False
+                    break 
+            if is_prime:
+                list.append(count)
+            count+=1
+        return list
+    else:
+        raise ValueError(f"{number_of_primes} should be greater than 0")
+
+ten_primes = primes(10)
+
+print(ten_primes)
